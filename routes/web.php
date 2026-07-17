7d2205cfd1868154f73d1f1a1404676ca06d544d
amon <rakabitornetwork@gmail.com>
2026-07-17 22:17:27 +0700

    Update storage link

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', LandingController::class)->name('home');
-
 Route::middleware('guest')->group(function () {
     Route::get('/admin/login', [AdminLoginController::class, 'create'])->name('admin.login');
     Route::post('/admin/login', [AdminLoginController::class, 'store']);
